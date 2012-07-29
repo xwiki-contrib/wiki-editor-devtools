44da702549ff3ea450a6dc87c9fa480c74bf9feb
Vincent Massol <vincent@massol.net>
2012-07-29 09:41:43 +0200

    First try at adding a functional test. ATM I cannot trigger autocompletion manually and I don't know why. To reproduce: run "mvn clean generate-test-resources" and then cd to target/xwiki and run "sh start_xwiki.sh", then go to any page in wiki edit mode and try it.

---
+++ xwiki-platform-editor-tool-autocomplete/xwiki-platform-editor-tool-autocomplete-tests/src/test/it/org/xwiki/editor/tool/autocomplete/test/ui/AutoCompleteTest.java
@@ -0,0 +1,45 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.editor.tool.autocomplete.test.ui;
+
+import org.junit.Test;
+import org.xwiki.test.ui.AbstractTest;
+import org.xwiki.test.ui.po.ViewPage;
+import org.xwiki.test.ui.po.editor.WikiEditPage;
+
+/**
+ * Tests autocompletion in the Velocity macro while being in the wiki editor.
+ *
+ * @version $Id$
+ * @since 4.2M2
+ */
+public class AutoCompleteTest extends AbstractTest
+{
+    @Test
+    public void autoComplete() throws Exception
+    {
+        // Create test page in which we're goign to test autocompletion
+        ViewPage vp = getUtil().createPage(getClass().getSimpleName(), getTestMethodName(), "", "AutoCompletion Test");
+        WikiEditPage wep = vp.editWiki();
+        wep.setContent("{{velocity}}$");
+        // Trigger autocompletion!
+
+    }
+}

da5c07dd2a0b3d30327ad26bde0efc1f03f24e0c
Eduard Moraru <Enygma2002@gmail.com>
2017-08-21 18:58:51 +0300

    [build] Not skipping the enforcer plugin.

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-PARAMETERS="-DskipTests=true -Dxwiki.enforcer.skip=true"
+PARAMETERS="-DskipTests=true"
 
 mvn release:prepare -U ${PARAMETERS} -DautoVersionSubmodules=true -Darguments="${PARAMETERS}" && mvn release:perform ${PARAMETERS} -Darguments="${PARAMETERS}"

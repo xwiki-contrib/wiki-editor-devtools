62e620b6132d03f249a1d23c603744f79921b844
Vincent Massol <vincent@massol.net>
2021-04-15 15:52:40 +0200

    [Misc] Removing (wrong) script since contrib.xwiki.org is what needs to be followed

--- release.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-PARAMETERS="-DskipTests=true"
-
-mvn release:prepare -U ${PARAMETERS} -DautoVersionSubmodules=true -Darguments="${PARAMETERS}" && mvn release:perform ${PARAMETERS} -Darguments="${PARAMETERS}"
